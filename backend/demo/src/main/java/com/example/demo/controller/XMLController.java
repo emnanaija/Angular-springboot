0ab0bad3c7e09aea9bd8000c7ff01eddd7428312
emna-naija-pro <emnanaija@gmail.com>
2024-08-04 11:21:07 +0100

    validation xml xith xsd successful

--- backend/demo/src/main/java/com/example/demo/controller/XMLController.java
+++ backend/demo/src/main/java/com/example/demo/controller/XMLController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.Retenue_four;
-import com.example.demo.dao.RetenuFourRepository;
 import com.example.demo.service.XMLGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -11,32 +9,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api")
 public class XMLController {
-
-    @Autowired
-    private RetenuFourRepository retenueFourRepository;
 
     @Autowired
     private XMLGeneratorService xmlGeneratorService;
 
     @GetMapping("/generate-xml")
     public ResponseEntity<String> generateXML() {
-        List<Retenue_four> retenueFours = retenueFourRepository.findAll();
-        if (retenueFours.isEmpty()) {
-            return new ResponseEntity<>("No records found!", HttpStatus.OK);
-        }
-        String xmlOutput = xmlGeneratorService.generateXML(retenueFours);
-        if (xmlOutput != null) {
+        String xmlOutput = xmlGeneratorService.generateXML();
+        if (xmlOutput.startsWith("XML validation failed")) {
+            return new ResponseEntity<>(xmlOutput, HttpStatus.INTERNAL_SERVER_ERROR);
+        } else if (xmlOutput.equals("Error generating XML")) {
+            return new ResponseEntity<>(xmlOutput, HttpStatus.INTERNAL_SERVER_ERROR);
+        } else {
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(org.springframework.http.MediaType.APPLICATION_XML);
             headers.setContentDispositionFormData("attachment", "declarants.xml");
+            headers.add("Validation-Status", "XML validation succeeded!");
             return new ResponseEntity<>(xmlOutput, headers, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("Error generating XML", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- backend/demo/src/main/java/com/example/demo/service/XMLGeneratorService.java
+++ backend/demo/src/main/java/com/example/demo/service/XMLGeneratorService.java
@@ -1,57 +1,114 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.Retenue_four;
+import com.example.generated.DeclarationsRS;
+import com.example.generated.TypeMatriculeFiscal;
+import com.example.generated.TypeReferenceDeclaration;
+import com.example.generated.TypeCategoriePersonne;
 import org.springframework.stereotype.Service;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.XMLConstants;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
 import java.io.StringWriter;
-import java.util.List;
+
+import org.xml.sax.SAXException;
 
 @Service
 public class XMLGeneratorService {
 
-    public String generateXML(List<Retenue_four> retenueFours) {
+    public String generateXML() {
         try {
-            DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
-            Document document = documentBuilder.newDocument();
+            // Générer une partie de l'objet DeclarationsRS
+            DeclarationsRS declarationsRS = new DeclarationsRS();
+            fillDeclarationsRS(declarationsRS);
 
-            // Root element
-            Element root = document.createElement("Declarants");
-            document.appendChild(root);
+            // Convertir l'objet en XML
+            JAXBContext jaxbContext = JAXBContext.newInstance(DeclarationsRS.class);
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
-            for (Retenue_four retenue : retenueFours) {
-                // Declarant element
-                Element declarant = document.createElement("Declarant");
+            StringWriter xmlWriter = new StringWriter();
+            marshaller.marshal(declarationsRS, xmlWriter);
+            String xmlString = xmlWriter.toString();
 
-                // Add value based on FRT_TYPIDENT
-                if (retenue.getFrtTypIdent() == 1) {
-                    declarant.appendChild(document.createTextNode("1"));
-                } else {
-                    declarant.appendChild(document.createTextNode(String.valueOf(retenue.getFrtMatcin())));
-                }
-
-                root.appendChild(declarant);
+            // Valider le XML généré par rapport au schéma XSD
+            String validationMessage = validateXML(xmlString);
+            System.out.println(validationMessage); // Afficher le message de validation dans la console
+            if (validationMessage.equals("XML validation succeeded!")) {
+                return xmlString;
+            } else {
+                return validationMessage;
             }
-
-            // Transform document to string
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            StringWriter writer = new StringWriter();
-            transformer.transform(new DOMSource(document), new StreamResult(writer));
-
-            return writer.getBuffer().toString();
 
         } catch (Exception e) {
             e.printStackTrace();
-            return null;
+            return "Error generating XML";
         }
     }
+
+
+
+    private void fillDeclarationsRS(DeclarationsRS declarationsRS) {
+        declarationsRS.setVersionSchema("1.0");
+
+        TypeMatriculeFiscal declarant = new TypeMatriculeFiscal();
+        declarant.setTypeIdentifiant("1");
+        declarant.setIdentifiant("1234567A");
+
+        // Définir la valeur de l'énumération TypeCategoriePersonne
+        declarant.setCategorieContribuable(TypeCategoriePersonne.PM);
+
+        declarationsRS.setDeclarant(declarant);
+
+        TypeReferenceDeclaration referenceDeclaration = new TypeReferenceDeclaration();
+        referenceDeclaration.setActeDepot("0");
+        referenceDeclaration.setAnneeDepot("2024");
+        referenceDeclaration.setMoisDepot("08");
+        declarationsRS.setReferenceDeclaration(referenceDeclaration);
+
+        // Ne pas remplir les autres champs pour l'instant
+    }
+
+    private String validateXML(String xmlString) {
+        try {
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(new File("C:\\Users\\chemseddine\\Desktop\\stage3eme\\backend\\demo\\src\\main\\resources\\xsd\\TEJDeclarationRS_v1.0.xsd"));
+
+            javax.xml.validation.Validator validator = schema.newValidator();
+
+            // Ajout d'un ErrorHandler pour capturer les erreurs de validation
+            validator.setErrorHandler(new org.xml.sax.ErrorHandler() {
+                @Override
+                public void warning(org.xml.sax.SAXParseException exception) throws SAXException {
+                    System.out.println("Warning: " + exception.getMessage());
+                }
+
+                @Override
+                public void error(org.xml.sax.SAXParseException exception) throws SAXException {
+                    System.out.println("Error: " + exception.getMessage());
+                }
+
+                @Override
+                public void fatalError(org.xml.sax.SAXParseException exception) throws SAXException {
+                    System.out.println("Fatal error: " + exception.getMessage());
+                }
+            });
+
+            validator.validate(new StreamSource(new StringReader(xmlString)));
+            System.out.println("XML validation succeeded!");
+            return "XML validation succeeded!";
+        } catch (SAXException | IOException e) {
+            System.out.println("XML validation failed: " + e.getMessage());
+            return "XML validation failed: " + e.getMessage();
+        }
+    }
+
+
 }
